de37d8e72b527b7f5ceff5e879ea6b3739a9954e
OldMan <146049953+Valdric@users.noreply.github.com>
2025-06-09 09:56:22 +0700

    Dikerjakan Oleh Valdric Abirama P.D - 233040163
    Dikerjakan Oleh Valdric Abirama P.D - 233040163 mengerjakan bagian services

---
+++ src/services/ProductQueue.java
@@ -0,0 +1,52 @@
+package services;
+
+import entity.AntrianProduk;
+
+public class ProductQueue {
+    private AntrianProduk[] array;
+    private int capacity, front, rear, size;
+
+    public ProductQueue(int capacity) {
+        this.capacity = capacity;
+        this.array = new AntrianProduk[capacity];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() { return size == 0; }
+    public boolean isFull() { return size == capacity; }
+
+    public void enqueue(AntrianProduk produk) {
+        if (isFull()) {
+            System.out.println("Antrian penuh!");
+            return;
+        }
+        rear = (rear + 1) % capacity;
+        array[rear] = produk;
+        size++;
+    }
+
+    public AntrianProduk dequeue() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong!");
+            return null;
+        }
+        AntrianProduk temp = array[front];
+        front = (front + 1) % capacity;
+        size--;
+        return temp;
+    }
+
+    public void printQueue() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return;
+        }
+        System.out.println("Isi Antrian:");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % capacity;
+            System.out.println("- " + array[index].getProduk().getNama());
+        }
+    }
+}
